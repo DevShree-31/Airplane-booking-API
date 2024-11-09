1d287a07ec37060c2efb19a7eb82c5d45b5914e9
Shreyansh <patelshreyansh376@gmail.com>
2024-11-10 01:47:39 +0530

    added a new migration to add a constraint in a column of airport table '''npx sequelize migration:generate --name update-city-airport-constraint'''

---
+++ src/migrations/20241109194915-update-city-airport-constraint.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.addConstraint('Airports',{
+      fields:['cityID'],
+      type:'foreign key',
+      name:'custom_fkey_constraint_name',
+      references:{
+        table:'Cities',
+        field:'id'
+      },
+      onDelete: 'cascade',
+      onUpdate: 'cascade'
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.removeConstraint('Airports','custom_fkey_constraint_name')
+  }
+};
